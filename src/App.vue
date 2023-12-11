a8286dbfff1db6face9c4a0bcabb766eac5d034a
christian ventes <chrventes@gmail.com>
2023-12-10 21:15:19 -0500

    Frontend Expanding tabs component

--- src/App.vue
+++ src/App.vue
@@ -26,5 +26,9 @@ export default {
   flex-direction: row;
 
 }
+.TabBillinfo{
+  border: dashed red;  
+
+}
 
 </style>
--- src/components/TabBillinfo.vue
+++ src/components/TabBillinfo.vue
@@ -9,17 +9,49 @@ export default defineComponent({
 </script>
 
 <template>
-    <div class="container-tab"> <p>Lo de adentro</p></div>
-   
+    
+    <div class="container-tab"> 
+
+        <div class = "tab"> Tab  </div>
+        <div class = "tab"> Tab  </div>
+        <div class = "tab"> Tab  </div>
+        <div class = "tab"> Tab  </div>
+    
+    </div>
+    
 </template>
 
 <style scoped>
 .container-tab {
 
-    background-color:lightgreen;
-    width: 500px;
-    height: 25px;
+    
+    width: 1000px;
+    height: auto;
+    border-radius: 5px;
+    border: 2px none #191a18;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    display:flex;
+    
+    
+    flex-direction: column;
+    
 }
+
+.tab{
+    margin-left: 2px;
+    border-radius: 2px;
+    border: 2px none #191a18;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    display:flex;
+    height: 25px;
+    
+}
+
+.tab:active{
+
+    height: 100px;
+}
+
 
 
 
